d138627e0f40fe2dc4c8a3627e800e54a6bbccce
joaq cid <joaqcid@gmail.com>
2018-12-11 17:16:43 -0300

    firebase deploy hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA7_xAgojzFclDGZI09mG0wnPPW3jT8viw",
+    authDomain: "employees-admin.firebaseapp.com",
+    databaseURL: "https://employees-admin.firebaseio.com",
+    projectId: "employees-admin",
+    storageBucket: "employees-admin.appspot.com",
+    messagingSenderId: "591835464901"
+  }
 };
