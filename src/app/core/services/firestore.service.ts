4d01bc4d09a0edb1e34e423534a0e809329d0fb0
Joaquin Cid <joaqcid@gmail.com>
2020-03-28 10:20:58 -0300

    ng update @angular/core

--- src/app/core/services/firestore.service.ts
+++ src/app/core/services/firestore.service.ts
@@ -1,9 +1,10 @@
-import { Inject } from "@angular/core";
+import { Inject, Injectable } from "@angular/core";
 import { AngularFirestore, QueryFn } from "@angular/fire/firestore";
 import { Observable } from "rxjs";
 import { tap } from "rxjs/operators";
 import { environment } from "src/environments/environment";
 
+@Injectable()
 export abstract class FirestoreService<T> {
 
     protected abstract basePath: string;
